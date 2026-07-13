36aad042987ac553cc825f1f141969b90be6cd48
moosarehan <moosarehan4@gmail.com>
2026-07-13 13:04:21 +0500

    feat: implement initial FastAPI application with patient viewing route

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import json
+app=FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+
+    return data
+
+@app.get("/")
+def hello():
+    return {'message':'hello world !'}
+
+
+@app.get("/about")
+def about():
+    return {'message':'your in about page !'}
+
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
+
