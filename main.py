bc348cd63eb90926d0cf6dc5eb678d2b1d39224b
moosarehan <moosarehan4@gmail.com>
2026-07-15 12:11:12 +0500

    added pydantic model

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
 from fastapi import FastAPI,Path,HTTPException,Query
 import json
+from pydantic import BaseModel,Field,computed_field,model_validator
+from typing import Annotated,Literal
 app=FastAPI()
+
+class Patient(BaseModel):
+    id:Annotated[str,Field(...,description='id of patient',examples=['P001'])]
+    name:Annotated[str,Field(...,description='name of patient',examples=['moosa'])]
+    city:Annotated[str,Field(...,description='city of patient',examples=['faislabad'])]
+    age:Annotated[int,Field(...,description='age of patient',examples=['30'],gt=0)]
+    gender:Annotated[Literal['male','female','other'],Field(...,description='gender of patient')]
+    height:Annotated[float,Field(...,gt=0.0,description='height of patient in ms')]
+    weight:Annotated[float,Field(...,gt=0,description='weight of patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self)->float:
+        bmi=round(self.weight/(self.height**2),2)
+        return bmi
+
+    
+    @computed_field
+    @property
+    def verdict(self)->str:
+        if self.bmi < 18.5:
+            return 'underweight'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
+
+
+        
+    
+    
+        
+           
+        
+    
+    
+
 
 def load_data():
     with open('patients.json','r') as f:
@@ -30,6 +69,8 @@ def fetchpatient(patient_id:str=Path(...,description='id of patient in DB',examp
         return data[patient_id]
     raise HTTPException(status_code=404,detail='patient id not found in DB')
 
+
+
 @app.get('/sort')
 def sort_patients(sort_by:str=Query(...,detail='sort on basis of key'),order_by:str=Query('asc',detail='sort in either ascending or descending')):
     data=load_data()
@@ -40,14 +81,13 @@ def sort_patients(sort_by:str=Query(...,detail='sort on basis of key'),order_by:
     if order_by not in valid_sort:
         raise HTTPException(status_code=400,detail='choose valid order by option either asc or desc')
     if order_by =='desc':
-        order_by=False
-    else:
         order_by=True
+    else:
+        order_by=False
 
 
     sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=order_by)
     return sorted_data
-
 
 
 
